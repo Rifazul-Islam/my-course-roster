8a8e30342ed64b9c41caa451b70cc99752402089
Rifazul <rifazul50@gmail.com>
2023-09-14 19:51:47 +0600

    insert the json format data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 
 const Header = () => {
+
  return (
  <div>
-      <h1 className="text-4xl text-center font-semibold my-8" >Course Registration</h1>                                     
+      <h1 className="text-4xl text-center font-semibold my-8">Course Registration</h1>                                     
  </div>
 );
 };
