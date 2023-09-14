206b1034a089521dd7b573787280c1a22fccb570
Rifazul <rifazul50@gmail.com>
2023-09-14 19:13:41 +0600

    initial setup and header component make

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+ return (
+ <div>
+      <h1 className="text-4xl text-center font-semibold my-8" >Course Registration</h1>                                     
+ </div>
+);
+};
+
+export default Header;
